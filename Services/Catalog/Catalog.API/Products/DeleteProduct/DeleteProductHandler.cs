5a9a93cef0422ca5de9688f15efb7b6e00c66173
kiya <keyarash.karimi6@gmail.com>
2025-03-24 12:38:51 +0330

    Crud for product

---
+++ Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+
+    public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
+    public record DeleteProductResult(bool IsSuccess);
+
+  
+    internal class DeleteProductCommandHandler
+        (IDocumentSession session,ILogger<DeleteProductCommandHandler> logger)
+        : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+    {
+        public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("q3r4fv2");
+            session.Delete<Product>(command.Id);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new DeleteProductResult(true);
+        }
+    }
+}
---
+++ Services/Catalog/Catalog.API/Products/GetProduct/GetProductQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Catalog.API.Products.GetProduct
+{
+    public record GetProductQuery() : IQuery<GetProductResult>;
+    public record GetProductResult(IEnumerable<Product> Products);
+    internal class GetProductQueryHandler(IDocumentSession session, ILogger<GetProductQueryHandler> logger) : IQueryHandler<GetProductQuery, GetProductResult>
+    {
+        public async Task<GetProductResult> Handle(GetProductQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation($"get info {request}");
+            var product = await session.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductResult(product);
+        }
+    }
+}
