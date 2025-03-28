eeb0e518fc4a3eb873a995d385c0fb04b98e0daa
kiya <keyarash.karimi6@gmail.com>
2025-03-28 14:10:23 +0330

    InfrastructureOrderLayer

--- Services/Ordering/Ordering.API/Program.cs
+++ Services/Ordering/Ordering.API/Program.cs
@@ -1,18 +1,23 @@
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 namespace Ordering.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
             builder.Services.AddInfrastructureServices(builder.Configuration).
                 AddApplicationServices().
                 AddApiServices();
             var app = builder.Build();
-
+            app.UseApiServices();
+            if (app.Environment.IsDevelopment())
+            {
+                await app.InitialiseDatabaseAsync();
+            }
             app.MapGet("/", () => "Hello World!");
 
             app.Run();
